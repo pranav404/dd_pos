1258ede2e25ac254077c7ae248ea68304b7242c7
RushmithaP <91239495+RushmithaP@users.noreply.github.com>
2021-10-15 16:32:01 +0530

    cart bean

---
+++ src/main/java/com/dd_pos/bean/CartBean.java
@@ -0,0 +1,61 @@
+package com.pos.bean;
+import java.util.Date;
+public class CartBean {
+	
+	private int cartID;
+	private String userID;
+	private String foodID;
+	private String Type;
+	private int Quantity;
+	private double Cost;
+	private Date orderDate;
+	
+	public int getCartID() {
+		return cartID;
+	}
+	public void setCartID(int cartID) {
+		this.cartID = cartID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getFoodID() {
+		return foodID;
+	}
+	public void setFoodID(String foodID) {
+		this.foodID = foodID;
+	}
+	public String getType() {
+		return Type;
+	}
+	public void setType(String type) {
+		Type = type;
+	}
+	public int getQuantity() {
+		return Quantity;
+	}
+	public void setQuantity(int quantity) {
+		Quantity = quantity;
+	}
+	public double getCost() {
+		return Cost;
+	}
+	public void setCost(double cost) {
+		Cost = cost;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	
+}
+	
+
+
+
+
