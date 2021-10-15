17e2222ad62c96e504773991f349af7ef2d574bd
RushmithaP <91239495+RushmithaP@users.noreply.github.com>
2021-10-15 09:10:57 +0530

    Add files via upload
    foodbean

---
+++ src/main/java/com/dd_pos/bean/FoodBean.java
@@ -0,0 +1,54 @@
+package com.dd_pos.bean;
+
+public class FoodBean {
+	private String Foodid,Name,Type,Foodsize,Quantity,Price;
+
+	public String getFoodid() {
+		return Foodid;
+	}
+
+	public void setFoodid(String foodid) {
+		Foodid = foodid;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getType() {
+		return Type;
+	}
+
+	public void setType(String type) {
+		Type = type;
+	}
+
+	public String getFoodsize() {
+		return Foodsize;
+	}
+
+	public void setFoodsize(String foodsize) {
+		Foodsize = foodsize;
+	}
+
+	public String getQuantity() {
+		return Quantity;
+	}
+
+	public void setQuantity(String quantity) {
+		Quantity = quantity;
+	}
+
+	public String getPrice() {
+		return Price;
+	}
+
+	public void setPrice(String price) {
+		Price = price;
+	}
+
+}
